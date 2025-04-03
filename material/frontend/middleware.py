e9536a640423a2d74f041de350c5eb63a86800db
Adriano <a.palumbo@frankhood.it>
2025-04-03 12:29:15 +0200

    middleware inheritance

--- material/frontend/middleware.py
+++ material/frontend/middleware.py
@@ -1,4 +1,5 @@
 from django.http import QueryDict, HttpResponseRedirect
+from django.utils.deprecation import MiddlewareMixin
 
 try:
     from urllib.parse import urlencode, parse_qs, urlsplit, urlunsplit
@@ -7,23 +8,27 @@ except ImportError:
     from urllib import urlencode
 
 
-class SmoothNavigationMiddleware(object):
+class SmoothNavigationMiddleware(MiddlewareMixin):
     """Keep `?back=` queryset parameter on POST requests."""
 
     def process_response(self, request, response):  # noqa D102
         if isinstance(response, HttpResponseRedirect):
-            back = request.GET.get('back')
+            back = request.GET.get("back")
             if back:
                 _, _, back_path, _, _ = urlsplit(back)
-                scheme, netloc, path, query_string, fragment = urlsplit(response['location'])
+                scheme, netloc, path, query_string, fragment = urlsplit(
+                    response["location"]
+                )
                 query_params = parse_qs(query_string)
 
                 if path == back_path:
-                    query_params.pop('back', None)
-                elif 'back' not in query_params:
-                    query_params['back'] = [back]
+                    query_params.pop("back", None)
+                elif "back" not in query_params:
+                    query_params["back"] = [back]
 
                 new_query_string = urlencode(query_params, doseq=True)
-                response['location'] = urlunsplit((scheme, netloc, path, new_query_string, fragment))
+                response["location"] = urlunsplit(
+                    (scheme, netloc, path, new_query_string, fragment)
+                )
 
         return response
